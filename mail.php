f976877de09747f791c810c057c54b5bf3e88b8c
thomasrand <trand87@gmail.com>
2015-06-24 21:51:43 -0400

    content edits a PHP form.

---
+++ mail.php
@@ -0,0 +1,11 @@
+<?php 
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$message = $_POST['message'];
+	$formcontent="From: $name \n Message: $message";
+	$recipient = "trand87@gmail.com";
+	$subject = "Contact Form Message";
+	$mailheader = "From: $email \r\n";
+	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+	echo "Thank You!";
+?>
