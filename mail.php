d805084b2a4b8f5ae9ae480ed0f64d6de24b9b3e
thomasrand <trand87@gmail.com>
2015-06-24 22:22:39 -0400

    updated php mail script.

--- mail.php
+++ mail.php
@@ -2,10 +2,12 @@
 	$name = $_POST['name'];
 	$email = $_POST['email'];
 	$message = $_POST['message'];
-	$formcontent="From: $name \n Message: $message";
-	$recipient = "trand87@gmail.com";
+	$company = $_POST['company'];
+	$phone = $_POST['phone'];
+	$formcontent="From: $name \n Email: $email \n Company: $company \n Phone: $phone \n  Message: $message";
+	$recipient = "contactus@claimsbureau.com";
 	$subject = "Contact Form Message";
-	$mailheader = "From: $email \r\n";
+	$mailheader = "From: no-reply@claimsbureau.com \r\n";
 	mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-	echo "Thank You!";
+	echo "Thank You!  Message sent.";
 ?>
