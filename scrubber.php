d81d5178f1b9557bd624ce9cbed5e6b398103c22
Tom Rand <trand87@gmail.com>
2015-07-30 17:39:07 -0400

    updated history info.

--- scrubber.php
+++ scrubber.php
@@ -11,59 +11,51 @@
 	$mailheader = "From: no-reply@claimsbureau.com\r\n";
 
 
-	console.log(vars);
+	// Scrub inputs for bad characters and replace with ''
+	function spam_scrubber($value) {
 
-		// Scrub inputs for bad characters and replace with ''
-		function spam_scrubber($value) {
+		$bad_chars = array(
+			'to:',
+			'cc:',
+			'bcc',
+			'content-type:',
+			'mime-version:',
+			'multipart-mixed:',
+			'content-tranfer-encoding:');
 
-			$bad_chars = array(
-				'to:',
-				'cc:',
-				'bcc',
-				'content-type:',
-				'mime-version:',
-				'multipart-mixed:',
-				'content-tranfer-encoding:');
-
-			foreach($bad_chars as $v) {
-				if (stripos($value, $v) !== false) {
-					return '';
-				}
+		foreach($bad_chars as $v) {
+			if (stripos($value, $v) !== false) {
+				return '';
 			}
-
-			$value = str_replace(array("\r", "\n", "%0a", "%0d"), '', $value);
-
-			return trim($value);
-
 		}
 
-		// Call the srub function on form data
-		$scrubbed = array_map('spam_scrubber', $_POST);
+		$value = str_replace(array("\r", "\n", "%0a", "%0d"), '', $value);
 
-		// Basic form validation
-		if(!empty($scrubbed['$name']) && !empty($scrubbed['$email']) && !empty($scrubbed['$message']) ) {
+		return trim($value);
 
-			$formcontent = "Name: {$scrubbed['$name']}
-			\nEmail: {$scrubbed['$email']}  
-			\nCompany: {$scrubbed['$company']} 
-			\nPhone: {$scrubbed['$phone']}
-			\nMessage: {$scrubbed['$message']}"
+	}
 
-			console.log('mailed');
+	// Call the srub function on form data
+	$scrubbed = array_map('spam_scrubber', $_POST);
 
-			mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
-			echo 'Thank You!  Message sent';
+	// Basic form validation
+	if(!empty($scrubbed['$name']) && !empty($scrubbed['$email']) && !empty($scrubbed['$message']) ) {
 
-			$_POST = array();
+		$formcontent = "Name: {$scrubbed['$name']}
+		\nEmail: {$scrubbed['$email']}  
+		\nCompany: {$scrubbed['$company']} 
+		\nPhone: {$scrubbed['$phone']}
+		\nMessage: {$scrubbed['$message']}"
 
-		} else {
-			echo '<p>Please complete the requied fields.</p>';
-		}
+		
 
-	
+		mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
+		echo 'Thank You!  Message sent';
 
+		$_POST = array();
 
-
-	
-	
+	} else {
+		echo '<p>Please complete the requied fields.</p>';
+	}
+		
 ?>
